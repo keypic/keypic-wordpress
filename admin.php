3fdca949b70d1aff51c99b3cd09bdfe4bd30baff
Emanuele Rogledi <info@keypic.com>
2014-08-26 14:56:22 +0200

    1.8.0
    1.8.0

--- admin.php
+++ admin.php
@@ -1,18 +1,17 @@
 <?php
-add_action( 'admin_menu', 'keypic_config_page', 8);
+add_action('admin_menu', 'keypic_config_page', 8);
 
 add_action('admin_init', 'keypic_admin_init');
 
 $ms = array();
 
 $messages = array(
-	'key_empty' => array('color' => 'aa0', 'text' => sprintf(__('Your FormID is empty (<a href="%s" style="color:#fff">Get your FormID.</a>)'), 'http://keypic.com/modules/register/')),
+	'key_empty' => array('color' => 'aa0', 'text' => sprintf(__('Your FormID is empty, This plugin does not work without FormID, please (<a href="%s" target="_blank" style="color:#fff">Get your FormID.</a>)'), 'http://keypic.com/?action=register')),
 	'key_valid' => array('color' => '4AB915', 'text' => __('This FormID is valid.')),
 	'key_not_valid' => array('color' => 'aaa', 'text' => __('This FormID is NOT valid.')),
 );
 
-$plugins = array
-(
+$plugins = array(
 		// Wordpress builtin plugins
 		'register' => array('Name' => 'Registration Form details', 'builtin' => 1),
 		'login' => array('Name' => 'Login Form details', 'builtin' => 1),
@@ -29,6 +28,9 @@ function keypic_admin_init()
 
 	// all admin functions are disabled in old versions
 
+    
+    //add_action( 'admin_menu', array( 'Akismet_Admin', 'admin_menu' ), 5 ); # Priority 5, so it's called before Jetpack's admin_menu.
+
 	if(!function_exists('is_multisite') && version_compare($wp_version, '3.0', '<' ))
 	{
 		function keypic_version_warning()
@@ -41,6 +43,8 @@ function keypic_admin_init()
 		add_action('admin_notices', 'keypic_version_warning');
 		return;
 	}
+
+    display_notice();
 }
 
 function keypic_config_page()
@@ -66,13 +70,13 @@ add_filter( 'plugin_action_links', 'keypic_plugin_action_links', 10, 2 );
 
 function keypic_report_spam_and_delete_comment()
 {
-	global $keypic_comments, $FormID;
+    $keypic_comments = get_option('keypic_comments');
 
 	if(!(isset($_GET['id']) || (isset($_REQUEST['action']) && 'keypic_report_spam_and_delete_comment' == $_REQUEST['action']))){return;}
 
 	$comment = $keypic_comments[$_GET['id']];
 
-	Keypic::reportSpam($comment['token']);
+    Keypic::reportSpam($comment['token']);
 	wp_delete_comment($_GET['id']);
 
 	wp_redirect( $_SERVER['HTTP_REFERER'] );
@@ -166,9 +170,9 @@ function keypic_conf()
 		foreach(plugins_list() as $k => $v)
 		{
 			$RequestType = isset($_POST[$k.'_requesttype']) ? $_POST[$k.'_requesttype'] : '';
-			$WeighthEight = isset($_POST[$k.'_weightheight']) ? $_POST[$k.'_weightheight'] : '';
+			$WidthHeight = isset($_POST[$k.'_widthheight']) ? $_POST[$k.'_widthheight'] : '';
 			$enabled = isset($_POST[$k.'_enabled']) ? $_POST[$k.'_enabled'] : '';
-			$keypic_details[$k] = array('RequestType' => $RequestType, 'WeighthEight' => $WeighthEight, 'enabled' => $enabled);
+			$keypic_details[$k] = array('RequestType' => $RequestType, 'WidthHeight' => $WidthHeight, 'enabled' => $enabled);
 		}
 
 		update_option('keypic_details', $keypic_details);
@@ -178,32 +182,37 @@ function keypic_conf()
 		if($FormID == ''){$ms[] = 'key_empty';}
 	}
 
+	// Form
 	echo '<h2>' . __('Keypic Configuration') . ' - Version ' . KEYPIC_VERSION .'</h2>';
 
-	// Form
-	echo	'<form name="formid" action="" method="post" style="margin: auto; width: 750px; ">';
+	echo '<div id="dashboard_recent_drafts" class="postbox">';
+	echo '<h3 class="hndle"><span>' . __('Keypic FormID Management') . '</span></h3>';
+	    echo '<div class="inside">';
 
-	echo '<p>' . __('For many people, <a href="http://keypic.com/" target="_blank">Keypic</a> will greatly reduce or even completely eliminate the comment and trackback spam you get on your site. If one does happen to get through, simply mark it as "spam" on the moderation screen and Keypic will learn from the mistakes. If you don\'t have an API FormID yet, you can get one at <a href="http://keypic.com/modules/register/" target="_blank">keypic.com</a>.') . '</p>';
+
+	echo	'<form name="formid" action="" method="post" style="margin: auto; width: 100%; ">';
+
+	echo '<p>' . __('For many people, <a href="http://keypic.com/" target="_blank">Keypic</a> is quite possibly the best way in the world to protect your blog from comment and trackback spam. It keeps your site protected from spam even while you sleep. To get started: <br /> 1) Click the "Activate" link to the left of this description, <br /> 2) <a href="http://keypic.com/?action=register" target="_blank">Sign up for a FormID</a>, and <br /> 3) Go to your Keypic configuration page, and save FormID key.') . '</p>';
 	echo '<h3><label for="key">' . __('Keypic FormID') . '</label></h3>';
 
 	foreach($ms as $m):
 		echo '<p style="padding: .5em; background-color: #' . $messages[$m]['color'] . '; color: #fff; font-weight: bold;">' . $messages[$m]['text'] . '</p>';
 	endforeach;
 
-	echo '<p><input id="key" name="formid" type="text" size="32" maxlength="32" value="' . $FormID . '" style="font-family: \'Courier New\', Courier, mono; font-size: 1.5em;" /> (' . __('<a href="http://keypic.com/modules/register/">What is this?</a>') . ')</p>';
-
-	if(isset( $invalid_key) && $invalid_key)
-	{
-		echo '<h3>' . __('Why might my key be invalid?') . '</h3>';
-		echo '<p>' . __('This can mean one of two things, either you copied the key wrong or that the plugin is unable to reach the Keypic servers, which is most often caused by an issue with your web host around firewalls or similar.') . '</p>';
-	}
+	echo '<p><input id="key" name="formid" type="text" size="32" maxlength="32" value="' . $FormID . '" style="font-family: \'Courier New\', Courier, mono; font-size: 1.5em;" /> (<a href="http://keypic.com/?action=register" target="_blank">' . __('get registered') . '</a>) or if you are just logged in <a href="http://keypic.com/?action=forms" target="_blank">' . __('Create a new FormID') . '</a></p>';
 
 	echo '<p class="submit"><input type="submit" name="input_submit" value="' . __('Update FormID &raquo;') . '" /></p>';
 	echo '<input type="hidden" name="submit1" value="submit1" />';
 	echo	'</form>';
 
+
+
+	    echo '</div>';
+	echo '</div>';
+
+
 	// Form
-	echo	'<form name="formlist" action="" method="post" style="margin: auto; width: 750px; ">';
+	echo	'<form name="formlist" action="" method="post" style="margin: auto; width: 100%; ">';
 
 	foreach(plugins_list() as $k => $v)
 	{
@@ -218,17 +227,18 @@ function keypic_conf()
 
 			if($keypic_details[$k]['enabled'] == 1)
 			{
-				$WeighthEight = isset($keypic_details[$k]['WeighthEight']) ? $keypic_details[$k]['WeighthEight'] : '';
+				$WidthHeight = isset($keypic_details[$k]['WidthHeight']) ? $keypic_details[$k]['WidthHeight'] : '';
 				$RequestType = isset($keypic_details[$k]['RequestType']) ? $keypic_details[$k]['RequestType'] : '';
 
-				echo 'WeightHeight: <br />';
-				echo keypic_get_select_weightheight($k.'_weightheight', $WeighthEight) . '<br />';
+				echo 'Width Height: <br />';
+				echo keypic_get_select_widthheight($k.'_widthheight', $WidthHeight) . '<br />';
 	
 				echo 'RequestType: <br />';
 				echo keypic_get_select_requesttype($k.'_requesttype', $RequestType) . '<br />';
 	
 				echo '<p>' . __('content preview') . '<br />';
-				echo Keypic::getIt($RequestType, $WeighthEight) . '</p>';
+				if($WidthHeight == '1x1'){echo 'Transparenti Pixel Active!';} // Little Hack :)
+				else{echo Keypic::getIt($RequestType, $WidthHeight) . '</p>';}
 			}
 
 			echo '</div>';
@@ -245,7 +255,7 @@ function keypic_conf()
 
 			if($keypic_details[$k]['enabled'] == 1)
 			{
-				echo $v['Name'] . ' is enabled and ready to working with Keypic <br />';
+				echo $v['Name'] . ' is enabled and ready to work with Keypic <br />';
 				echo '<br /><br />';
 				echo '<img src="/wp-content/plugins/keypic/screenshot-contact-form-7-1.png" /> <br />';
 				echo '<img src="/wp-content/plugins/keypic/screenshot-contact-form-7-2.png" /> <br />';
@@ -262,6 +272,38 @@ function keypic_conf()
 	echo '<p class="submit"><input type="submit" name="input_submit" value="' . __('Update options &raquo;') . '" /></p>';
 	echo '<input type="hidden" name="submit2" value="submit2" />';
 	echo	'</form>';
+}
+
+function display_notice()
+{
+    global $hook_suffix;
+
+//	if( $hook_suffix == 'plugins.php' && !Keypic::getFormID() )
+	if( !Keypic::getFormID() )
+	{
+        display_api_key_warning();
+    }
+
+}
+
+function display_api_key_warning()
+{
+?><div class="updated" style="padding: 0; margin: 0; border: none; background: none;">
+	<style type="text/css">
+.keypic_activate{min-width:825px;border:1px solid #4F800D;padding:5px;margin:15px 0;background:#83AF24;background-image:-webkit-gradient(linear,0% 0,80% 100%,from(#83AF24),to(#4F800D));background-image:-moz-linear-gradient(80% 100% 120deg,#4F800D,#83AF24);-moz-border-radius:3px;border-radius:3px;-webkit-border-radius:3px;position:relative;overflow:hidden}.keypic_activate .aa_a{position:absolute;right:10px;font-size:80px;color:#769F33;font-family:Georgia, "Times New Roman", Times, serif;z-index:1}.keypic_activate .aa_button{font-weight:bold;border:1px solid #029DD6;border-top:1px solid #06B9FD;font-size:15px;text-align:center;padding:9px 0 8px 0;color:#FFF;background:#029DD6;background-image:-webkit-gradient(linear,0% 0,0% 100%,from(#029DD6),to(#0079B1));background-image:-moz-linear-gradient(0% 100% 90deg,#0079B1,#029DD6);-moz-border-radius:2px;border-radius:2px;-webkit-border-radius:2px}.keypic_activate .aa_button:hover{text-decoration:none !important;border:1px solid #029DD6;border-bottom:1px solid #00A8EF;font-size:15px;text-align:center;padding:9px 0 8px 0;color:#F0F8FB;background:#0079B1;background-image:-webkit-gradient(linear,0% 0,0% 100%,from(#0079B1),to(#0092BF));background-image:-moz-linear-gradient(0% 100% 90deg,#0092BF,#0079B1);-moz-border-radius:2px;border-radius:2px;-webkit-border-radius:2px}.keypic_activate .aa_button_border{border:1px solid #006699;-moz-border-radius:2px;border-radius:2px;-webkit-border-radius:2px;background:#029DD6;background-image:-webkit-gradient(linear,0% 0,0% 100%,from(#029DD6),to(#0079B1));background-image:-moz-linear-gradient(0% 100% 90deg,#0079B1,#029DD6)}.keypic_activate .aa_button_container{cursor:pointer;display:inline-block;background:#DEF1B8;padding:5px;-moz-border-radius:2px;border-radius:2px;-webkit-border-radius:2px;width:266px}.keypic_activate .aa_description{position:absolute;top:22px;left:285px;margin-left:25px;color:#E5F2B1;font-size:15px;z-index:1000}.keypic_activate .aa_description strong{color:#FFF;font-weight:normal}
+	</style>
+	<form name="keypic_activate" action="plugins.php?page=keypic-key-config" method="POST">
+		<div class="keypic_activate">
+			<div class="aa_a">Keypic</div>
+			<div class="aa_button_container" onclick="document.keypic_activate.submit();">
+				<div class="aa_button_border">
+					<div class="aa_button"><?php esc_html_e('Activate your Keypic account', 'keypic');?></div>
+				</div>
+			</div>
+			<div class="aa_description"><?php _e('<strong>Almost done</strong> - activate your account and say goodbye to comment spam', 'keypic');?></div>
+		</div>
+	</form>
+</div><?php
 }
 
 
--- readme.txt
+++ readme.txt
@@ -1,83 +1,181 @@
-=== NO CAPTCHA Anti Spam ===
-Contributors: Keypic
-Tags: keypic, captcha, captchas, spam, antispam, free, contact, comments, registration, login, secure, form, forms, ad, ads, adv, advertising
+=== Anti Spam Protection without CAPTCHA powered by Keypic ===
+Contributors: keypic, PKMateur, cipy
+Author URI: http://keypic.com
+Tags: Akismet, anti-spam, antispam, anti spam protection, bbPress, buddypress, captcha, cf7, comment, comments, comment spam, contact form protection, contact form 7, form protection, login, login protection, security, signup protection, spam, spam prevention, spammers, web form protection, wordpress, keypic
 Requires at least: 3.0
-Tested up to: 3.5.1
-Stable tag: 1.2.1
+Tested up to: 3.9.2
+Stable tag: trunk
 License: GPLv2 or later
-Donate link: http://keypic.com/?tid=homepage_donations_not_needed
 
-Keypic checks your comments against the Keypic Web Service to see if they look like spam or not.
+Anti Spam Protection without CAPTCHA. Keypic checks your blogs comments against the comprehensive Keypic Web Service to see if they look like spam or not.
 
 == Description ==
 
-Keypic checks your forms against the Keypic Web Service to see if they look like spam or not and lets you
-review the spam it catches under your blog's "Comments" admin screen.
-Keypic protect also your login form from brute force dictionary attachs, your registration form from unwanted spammers and generally all the forms you can imagine :)
-If you want a complete free antispam solution, you don't want to install CAPTCHAs plugins and so on, this is the right choice.
-Is also possible look at live installation at [http://wordpress.keypic.com/](http://wordpress.keypic.com/)
+No CAPTCHA, No SPAM! Delivering more reliable anti spam protection, making your website more accessible.
+Keypic delivers cloud anti spam protection technology and you do need to use CAPTCHA plugins anymore.
+By the way, your users will much appreciate it.
 
-Major new features in Keypic 1.0 include:
 
-* Now is it possible activate single forms indipendently.
-* Contact Form 7 extension is included in keypic.
-* It is no more needed to be registered, Keypic function also without registration, but with some limitations.
-* Check of your FormID in Keypic Configuration
-* Only if you are registered is available the comment status history, so you can easily see which comments were caught or cleared by Keypic, and which were spammed or unspammed by a moderator
-* In the same way is possible users status history, so you can easily see which users were caught or cleared by Keypic.
-* If your web host is unable to reach Keypic's servers, the plugin will automatically retry until the connection is back.
+[youtube http://www.youtube.com/watch?v=E4WGxcYZ4x8]
 
-PS: You'll need an [Keypic FormID](http://keypic.com/) to get improved functionality.  Keys are free for every kind of use (It's free and will always be.)
+
+No unintelligible words, no silly questions, no riddles, no puzzles, no counting objects, no math, no wasting time, no spammers and no spam bots. Keypic takes care of your security. Keypic is the alternative to CAPCTHA. Keypic stands for safe Internet and accessibility.
+
+
+= Keypic anti spam features =
+
+* Prevents all spammers' activity
+* Prevents spam bots comments
+* Prevents spam bots signups
+* Prevents spam bots and ontacts emails
+* Prevents spam trackbacks
+
+Alongside with full anti spam protection you get User Engagement and Conversion Rate growing because your users do not need to suffer from CAPTCHA and your website becomes more friendly to users with disabilities.
+
+
+= Keypic Anti Spam Use Cases =
+
+
+Keypic Anti Spam protects
+
+* signup and login forms from focused hacking attempts;
+* registration forms from unwanted spammers and spam bots;
+* payment forms;
+* most other forms that you insert into your website.
+
+
+Keypic Anti Spam prevents
+
+* spam comments
+* spam registrations
+* spam contact mails
+
+
+= Keypic is the Alternative to CAPTCHA =
+
+If you are looking for a complete anti spam protection and you don't want to install plugins based on CAPTCHA technology  Keypic is the right choice for you.
+It is also possible to perform a live installation from [wordpress.keypic.com http://wordpress.keypic.com/]
+
+
+= Use Keypic Anti Spam Protection for Free and Get Paid =
+
+You can start using Keypic Anti Spam Protection for free.
+In this case we put an ad announcement in the form and every time when someone clicks that ad you get paid.
+If you do not want to show third party ads then you can subscribe to one of packages that is relevant to your needs.
+In this case you can put your ad, any image, video or nothing (there will be a single pixel invisible for human's eyes).
+
+= CAUTION! =
+
+Keypic Anti Spam Protection works only for Registered Users.
+Do not change code otherwise we will not guarantee full spam prevention.
+
+Activate single forms independently from each other.
+Keypic Anti Spam Protection plugin includes the Contact Form 7 extension.
+If your web host is unable to reach Keypic's servers, the plugin will automatically retry until the connection is restored.
+
+Access to comment status history.
+Easily see which comments were caught or cleared by Keypic, and which were spammed or unspammed by a moderator.
+Access to user status history, so you can easily see which users were caught or cleared by Keypic.
+
+
+= Language and translation =
+
+* English
+* More are needed... Please help translate.
+
+If you would like to create your own language pack or update the existing one, you can send <a href="http://codex.wordpress.org/Translating_WordPress" target="_blank">the text of PO and MO files</a> for <a href="http://keypic.com/?action=feedback" target="_blank">Keypic Feedback</a> and we'll add it to the plugin.
+You can download the latest version of the program for work with PO and MO files  <a href="http://www.poedit.net/download.php" target="_blank">Poedit</a>.
+
+
+= Technical support =
+
+Dear users, our Keypic Anti Spam Protection plugin is available for free download.
+If you have any questions or suggestions regarding the functionality of our plugins (existing options, new options, current issues), please feel free to contact us.
+Please note that we accept requests in English only.
+All messages in another languages won't be accepted.
+
+If you found any bugs in the Keypic Anti Spam Protection plugin, you can notify us about it and we'll investigate and fix the issue then.
+Your request should contain URL of the website, issues description and WordPress admin panel credentials.
+Send you inquiry to keypicwp@keypic.com
+
+
+
+Requirements/Restrictions:
+-------------------------
+ * Keypic Anti Spam Protection plugin works with WordPress 2.9+, WPMU, and BuddyPress (WordPress 3.6+ is highly recommended)
+ * PHP5+ is highly recommended
+ 
+ 
 
 == Installation ==
 
-Upload the Keypic plugin to your blog, Activate it, then enter your [Keypic FormID](http://keypic.com/modules/register/) (if you want it, is not obbligatory).
+Upload the Keypic plugin to your blog, Activate it, then [get registered on Keypic website](https://keypic.com/?action=register) (From version 1.5 is Obligatory!).
+
 
 == Frequently Asked Questions ==
 Ask some questions and we will be happy to answer!
-Just bring us a [feedback](http://keypic.com/modules/feedback/)
+Just bring us a [feedback](http://keypic.com/?action=feedback)
+
 
 == Screenshots ==
 
-1. How Keypic act on comments form.
-2. How Keypic act on login form.
+1. How Keypic act on login form.
+2. How Keypic act on lastpassword.
 3. How Keypic act on register form.
-4. How Keypic act on lastpassword.
+4. How Keypic act on comments form.
 5. How Keypic act on register form in case of spam.
+6. Keypic control panel without FormID.
+7. Keypic control panel with FormID.
+8. Keypic control panel with FormID properly installed.
+
 
 == Upgrade Notice ==
 A new version is waiting for next week
 
+
 == Changelog ==
 
-= 1.2.1 =
-* Enhancement: Default getScript method set to 125x125
+= 1.8.0 =
+* Enhancement: New API 1.9
+* Enhancement: Better activation screen
 
-= 1.2.0 =
-* Enhancement: Updated base Class to 1.5
-* Enhancement: Added new getScript method instead of getiFrame
-
-= 1.1.4 =
-* Enhancement: Added initial support for Grunion Contact Form
-* Enhancement: Removed some unused Advertising Formats
-
-= 1.1.3 =
-* Enhancement: Changed plugin bootstrap
-
-= 1.1.2 =
+= 1.7.6 =
 * Bug Fix: Minor bug fixed
 
-= 1.1.1 =
-* Enhancement: Updated class to version 1.4
+= 1.7.5 =
+* Bug Fix: Minor bug fixed
+* Enhancement: New screen-shots
 
-= 1.1.0 =
-* Enhancement: Better explanation about Contact Form 7 integration.
+= 1.7.4 =
+* Bug Fix: Minor bug fixed
 
-= 1.0.2 =
-* Bug Fix: Fixed little bug who inhibit login Form work properly.
+= 1.7.3 =
+* Bug Fix: Minor bug fixed
 
-= 1.0.1 =
-* Bug Fix: Fixed little bug who inhibit Contact Form 7 work properly.
+= 1.7.2 =
+* Bug Fix: Minor bug fixed
 
-= 1.0.0 =
-* Enhancement: Now is it possible activate single forms indipendently.
+= 1.7.1 =
+* Bug Fix: Minor bug fixed
+
+= 1.7.0 =
+* Enhancement: Integrated new advertising service
+
+= 1.6.0 =
+* Enhancement: New API 1.8
+
+= 1.5.3 =
+* Enhancement: New API 1.7
+* Enhancement: Added protocol independent feature 
+* Bug Fix: Minor bug fixed
+
+= 1.5.2 =
+* Bug Fix: Minor bug fixed
+
+= 1.5.1 =
+* Bug Fix: Minor bug fixed
+
+= 1.5.0 =
+* Enhancement: New API 1.6
+* Enhancement: New Keypic service
+* Enhancement: Registered only users
